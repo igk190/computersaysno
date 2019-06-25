6947f15d21b65a58eec74842fff1d134a2663137
igk190 <graciakleijnen@hotmail.com>
2019-06-25 16:27:31 +0200

    Add updates from 3 weeks ago..

--- popup.js
+++ popup.js
@@ -1,24 +1,38 @@
-let changeColor = document.getElementById('changeColor');
-let options = document.getElementById('options')
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color)
-})
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'}); 
-    });
+
+// chrome.storage.sync.get('color', function(data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color)
+// })
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'}); 
+//     });
+// };
+
+ 
+// function openIndex() {
+//     chrome.tabs.create( { url: "popup.html"} );
+// } ?? never runs, why do you need this?
+
+
+// let options = document.getElementById('go-to-options')
+// options.addEventListener('click', function() {
+//     chrome.runtime.openOptionsPage();
+// })
+function openOptionsPage() {
+    // chrome.runtime.openOptionsPage();
+    chrome.runtime.openOptionsPage(function() { console.log(' oh no it didnt work')})
 };
 
-function openOptionsPage() {
-    options.addEventListener('click', function() {
-        chrome.runtime.openOptionsPage();
-    })
-}
-openOptionsPage()
+document.getElementById('go-to-options').addEventListener('click',
+openOptionsPage);
 
 
+ 
+
+ 
